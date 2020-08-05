8c97335feeb8fd6a08135d53162f801db254449d
zayonenafjad <66731311+zayonenafjad@users.noreply.github.com>
2020-08-05 21:02:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+Recepie for pizza-
+
+dough-
+
+2-2 1/3 cups of all-purpose flour
+2 1/4 teaspoon instant yeast
+1 1/2 teaspoon sugar
+3/4 teaspoon Salt 
+1/8-1/4 teaspoon garlic powder or basil leaves (optional) 
+2 tablespoons olive oil + additional
+3/4 cup warm water (make sure its not too hot nor too cold)
+
+
+instructions-
+
+In a bowl,combine only 1 cup of flour,instant yeast,sugar,salt and garlic powder/ basil leaves.
+add olive  oil and warm water and use a wooden spoon to stir very well.
+gradually add another 1 cup of flour,add the additional 1/3 cup of flour if too wet.
+stir until the dough is formed.
+
+drizzle a seperate ,large,clean bowl with the additional olive oil and brush the sides of the bowl.
+form the pizza dough into a ball and put it inside the bowl which was brushed with olive oil and roll
+the dough around the bowl.
+ then cover the bowl tightly with a plastic wrap and set it aside in a warm place for half an hour
+ or until it doubles in size.
+
+ after it is completely risen,deflate it and wrap the bowl again and store in refrigerator if 
+ using it later or bake right away
+
+
+
+
+ marinara/pizza sauce-
+
+take 3 ripe tomatoes and boil it in water.
+take the tomatoes out when the skin starts to fall apart.
+after it is cooled down,blend all the three tomatoes in a blender.
+in a fry pan, heat 1 tablespoon of olive oil.
+add finely chopped, 1 clove of garlic and saute it until it turns golden brown,
+add 1 finely chopped,small onion and saute until it is golden brown,
+add the prepared tomato pulp,1 teaspoon oregano powder,1 1/2 teaspoon chilli powder,
+2 tablespoons tomatoe sauce, 1/2 teaspoon salt and 1 teaspoon chilli flakes, stir until well
+combined.
+your marinara/pizza sauce is READY!!!
+
+
+final steps-
+
+take out your pizza dough from the refrigerator and roll it into desired shape and thickness,
+form desired crust, add marinara/pizza sauce, add desired toppings, preheat oven to 200 degree celsius
+for 5 mins, put in your pizza and cook until crust is hard.
+
+YOUR PIZZA IS READY!!!!
+
+
+
+
+
+
+
+
